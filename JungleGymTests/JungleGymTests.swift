d6bfa1d42124be6f6aa81ffc3a429c7642a98117
Brandon Evans <brandon@brandonevans.ca>
2018-01-29 18:53:58 -0700

    Add test script

---
+++ JungleGymTests/JungleGymTests.swift
@@ -0,0 +1,12 @@
+//
+//  JungleGymTests.swift
+//  JungleGymTests
+//
+//  Created by Brandon Evans on 2018-01-29.
+//  Copyright © 2018 Brandon Evans. All rights reserved.
+//
+
+import XCTest
+
+class JungleGymTests: XCTestCase {
+}
